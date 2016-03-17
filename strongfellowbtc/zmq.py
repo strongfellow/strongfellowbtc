f9b8f8ef9da5c1ec171d1fd3901ee1a3b9ce7c7a
strongfellow <a@b.com>
2016-03-16 20:12:14 -0700

    delete unneeded code

--- strongfellowbtc/zmq.py
+++ strongfellowbtc/zmq.py
@@ -14,21 +14,3 @@ class socket():
 
     def __exit__(self, exc_type, exc_val, exc_tb):
         self._context.destroy()
-    
-def generate_zmq_messages(port, *topics):
-    port = 28332
-    zmqContext = zmq.Context()
-    for t in topics:
-        zmqSubSocket.setsockopt(zmq.SUBSCRIBE, t)
-    zmqSubSocket.connect("tcp://127.0.0.1:%i" % port)
-
-
-    try:
-        while True:
-            msg = zmqSubSocket.recv_multipart()
-            topic = str(msg[0])
-            body = msg[1]
-            yield (topic, body)
-        except:
-            zmqContext.destroy()
-            raise
