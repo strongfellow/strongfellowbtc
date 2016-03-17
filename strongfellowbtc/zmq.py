ef2af46fef4fa36310c6560702285e31a5758513
strongfellow <a@b.com>
2016-03-16 20:22:03 -0700

    correct references

--- strongfellowbtc/zmq.py
+++ strongfellowbtc/zmq.py
@@ -6,12 +6,14 @@ import zmq
 class socket():
     def __init__(self, port, topic):
         self._context = zmq.Context()
+        self._port = port
+        self._topic = topic
 
 
     def __enter__(self):
         socket = self._context.socket(zmq.SUB)
-        socket.setsockopt(zmq.SUBSCRIBE, topic)
-        socket.connect("tcp://127.0.0.1:%i" % port)
+        socket.setsockopt(zmq.SUBSCRIBE, self._topic)
+        socket.connect("tcp://127.0.0.1:%i" % self._port)
         return socket
 
     def __exit__(self, exc_type, exc_val, exc_tb):
