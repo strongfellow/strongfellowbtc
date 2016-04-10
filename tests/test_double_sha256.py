1e92cac33228e50a897cc9678247128454664818
strongfellow <a@b.com>
2016-04-10 09:58:52 -0700

    rework test to use protocol module

--- tests/test_double_sha256.py
+++ tests/test_double_sha256.py
@@ -1,8 +1,8 @@
 
 import nose.tools
 
+from  strongfellowbtc.protocol import ds256
 import strongfellowbtc.hex as hex
-import strongfellowbtc.hash as hash
 
 def test_switch_endian():
     little_endian_hex = '9595c9df90075148eb06860365df33584b75bff782a510c6cd4883a419833d50'
@@ -15,7 +15,7 @@ def test_switch_endian():
     nose.tools.eq_(little_endian_hex, hex.switch_endian(big_endian_hex))
 
 def test():
-    h = hash.double_sha256('hello')
+    h = ds256('hello')
 
     little_endian_hex = '9595c9df90075148eb06860365df33584b75bff782a510c6cd4883a419833d50'
     big_endian_hex = '503d8319a48348cdc610a582f7bf754b5833df65038606eb48510790dfc99595'
@@ -23,3 +23,4 @@ def test():
 
     nose.tools.eq_(little_endian_hex, hex.little_endian_hex(h))
     nose.tools.eq_(big_endian_hex, hex.big_endian_hex(h))
+    nose.tools.eq_(type(h), type('andrew'))
