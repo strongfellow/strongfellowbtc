f37fef9b9153451ecbd78017a92109c067086a7d
strongfellow <a@b.com>
2016-05-02 21:00:37 -0700

    simplify poster, lets do the logic on the backend

--- strongfellowbtc/txpost.py
+++ strongfellowbtc/txpost.py
@@ -1,34 +1,30 @@
 
+from __future__ import absolute_import
+
 import argparse
-import msgpack
+import logging
 import requests
 import sys
 
-from datetime import datetime
-
 import strongfellowbtc.zmq
-import strongfellowbtc.constants as constants
+from strongfellowbtc.logging import configure_logging
+from strongfellowbtc.protocol import ds256
+from strongfellowbtc.hex import big_endian_hex
 
 def post_incoming_transactions(args=None):
     if args is None:
         args = sys.argv[1:]
     parser = argparse.ArgumentParser()
-    parser.add_argument('--host', required=True)
-    parser.add_argument('--network', required=True)
     parser.add_argument('--txport', type=int, default=str(constants.RAW_TX_PORT))
 
     params = parser.parse_args(args)
     
     with strongfellowbtc.zmq.socket(port=params.txport, topic='rawtx') as socket:
         while True:
+            logging.info('receiving transactions from zeromq')
             topic, tx = socket.recv_multipart()
-            delta = datetime.now() - datetime(1970, 1, 1)
-            ms = long(delta.total_seconds() * 1000)
-            data = msgpack.packb({
-                't': ms, # milliseconds since epoch
-                'x': tx, # the transaction
-                'h': params.host, # short name of the host
-                'n': params.network # main, testnet, segnet, etc.
-            })
-            r = requests.post('http://localhost:8080/transactions', data=data)
-            print r.status_code
+            h = big_endian_hex(ds256(tx))
+            url = 'http://localhost:8080/transactions/%s' % h
+            logging.info('received tx, posting to %s', url)
+            r = requests.post(url, data=tx)
+            logging.info('posted tx %s with status code %d', h, r.status_code)
